16e1690b3b0a2ca54bf7b8612744b5310221c4e9
florybal <soonluck8@gmail.com>
2023-10-24 23:40:34 -0300

    alterações no tamanho do sprite do personagem

--- src/game.cpp
+++ src/game.cpp
@@ -29,7 +29,7 @@ void game::run()
 
 void game::clean()
 {
-
+    std::cout << "Cleaning game\n";
     SDL_DestroyRenderer(_render);
     SDL_DestroyWindow(_window);
     SDL_Quit();
@@ -45,9 +45,9 @@ void game::render()
 {
     SDL_RenderClear(_render);
 
-    m_textureManager.draw("personagem", 0, 0, 168, 216, _render);
+    m_textureManager.draw("personagem", 0, 0, 100, 100, _render);
 
-    m_textureManager.drawFrame("personagem", 0, 0, 168, 216, 1, m_currentFrame, _render, SDL_FLIP_HORIZONTAL);
+    m_textureManager.drawFrame("personagem", 0, 0, 10, 10, 1, m_currentFrame, _render, SDL_FLIP_NONE);
 
     SDL_SetRenderDrawColor(_render, 96, 128, 150, 100);
     SDL_RenderPresent(_render);
@@ -55,19 +55,17 @@ void game::render()
 
 void game::init()
 {
-
     isRuning = true;
 
     int rendererFlags = SDL_RENDERER_ACCELERATED;
-    int windowFlags   = 0;
+    //int windowFlags   = 0;
 
     _window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                               screen_Width, screen_Height, windowFlags);
+                               screen_Width, screen_Height, SDL_WINDOW_RESIZABLE);
 
     if (SDL_Init(SDL_INIT_VIDEO)< 0)
         std::cout<<"nao foi possivel inicializar o SDL: \n" << SDL_GetError();
     
-
     if(!_window)
         std::cout<<"nao foi possivel criar a janela: \n"
                  << screen_Width, SDL_GetError();
